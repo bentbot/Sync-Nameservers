e80203656ccafd971803c9224a69b1f0e8c8edba
Liam Hogan <liam@hogan.re>
2021-10-30 14:42:04 -0400

    Instant WHM DNS Fixer
    Now Refactored! 🤓
    
    - cPanel IP Address auto-updates!
    - No Internet/IP detection
    - Automatic DNS record backups to a folder (./dnsbak, ./dnsbak2, ...)
    - Serial number corrector
    - Instant DNS refresh + correction (60s timer)
    
    1. Place script in "/srv/scripts/" folder.
    2. Make permissions work: $ chmod +rwx /srv/scripts/*
    3. Replace "YOUR_MAIN_DOMAIN.com.db" with your public nameserver / server hostname.

--- dnsupdate.sh
+++ dnsupdate.sh
@@ -1,39 +1,54 @@
-IP=`cat ./lastip.txt`;
+#find /srv/scripts/dnsbak/YOUR_MAIN_DOMAIN.com.db -mtime -1 -exec perl -pi -e 'if (/^\s+(\d{10})\s+;Serial Number/i) { my $i = $1+1; s/$1/$i/;}' '{}' \;
+IP=`cat /srv/scripts/lastip.txt`;
 #echo $IP;
 NEWIP=`curl -L --silent http://cpanel.net/showip.cgi`
 #echo $NEWIP;
+FILE=/var/named/YOUR_MAIN_DOMAIN.com.db;
+if [ ! -f "$FILE" ]; then
+echo 'Restoring DNS Mainfile...';
+sed -i "s/PUBLICIPADDR/$NEWIP/g" /srv/scripts/sc;
+\cp /srv/scripts/sc /var/named/YOUR_MAIN_DOMAIN.com.db;
+fi
 if [ -z "${NEWIP}" ]; then
 echo 'No new IP found. Internet connection ERROR';
 echo 'Exiting...';
 exit 0;
 fi
 if [ -z "${IP}" ]; then
-echo 'No old IP found. Please add the existing NS IP address to ./lastip.txt';
+echo 'No old IP found. Please add the existing NS IP address to /srv/scripts/lastip.txt';
 echo 'Exiting...';
 exit 0;
 fi
 if [ $NEWIP == $IP ]; then
 echo $IP;
 echo 'OK - DNS up to date.';
+service named restart;
 echo 'Exiting...';
 exit 0;
 fi
 echo $NEWIP;
 echo 'New IP Detected. Reloading DNS...';
-php ./autoip.php;
-cp /var/named/*.db ./dnsbak2;
-cp /var/named/*.db ./dnsbak;
+#sed -i "s/PUBLICIPADDR/$NEWIP/g" /srv/scripts/sc;
+#\cp /srv/scripts/sc /var/named/YOUR_MAIN_DOMAIN.com.db;
+php /srv/scripts/namedotcomapi/autoip.php;
+cp /var/named/*.db /srv/scripts/dnsbak2;
+cp /var/named/*.db /srv/scripts/dnsbak;
 rm /var/cpanel/nameserverips.cache;
-sed -i "s/$IP/$NEWIP/g" ./dnsbak/*;
+sed -i "s/$IP/$NEWIP/g" /srv/scripts/dnsbak/*;
 sed -i "s/$IP/$NEWIP/g" /var/cpanel/nameserverips.yaml;
-find ./dnsbak/*.db -mtime -1 -exec perl -pi -e 'if (/^\s+(\d{10})\s+;\s+serial/i) { my $i = $1+1; s/$1/$i/;}' '{}' \;
-find ./dnsbak/*.db -mtime -1 -exec perl -pi -e 'if (/^\s+(\d{10})\s+;Serial Number/i) { my $i = $1+1; s/$1/$i/;}' '{}' \;
-\cp ./dnsbak/*.db /var/named/;
+find /srv/scripts/dnsbak/*.db -mtime -1 -exec perl -pi -e 'if (/^\s+(\d{10})\s+;\s+serial/i) { my $i = $1+1; s/$1/$i/;}' '{}' \;
+find /srv/scripts/dnsbak/*.db -mtime -1 -exec perl -pi -e 'if (/^\s+(\d{10})\s+;Serial Number/i) { my $i = $1+1; s/$1/$i/;}' '{}' \;
+find /srv/scripts/dnsbak/YOUR_MAIN_DOMAIN.com.db -mtime -1 -exec perl -pi -e 'if (/^\s+(\d{10})\s+;Serial Number/i) { my $i = $1+1; s/$1/$i/;}' '{}' \;
+\cp /srv/scripts/dnsbak/*.db /var/named/;
 chown named:named /var/named/*.db;
 chmod +rwx /var/named/*.db;
-rm ./lastip.txt;
-echo $NEWIP > ./lastip.txt;
-/usr/local/cpanel/scripts/updatenameserverips;
+rm /srv/scripts/lastip.txt;
+echo $NEWIP > /srv/scripts/lastip.txt;
+/usr/local/cpanel/scripts/updatenameserverips 2>&1;
 service named restart;
 service cpanel restart;
 echo 'DNS Reloaded.';
+echo 'Waiting 1m...';
+sleep 1m;
+echo 'Rebooting Nameservers';
+service named restart;
